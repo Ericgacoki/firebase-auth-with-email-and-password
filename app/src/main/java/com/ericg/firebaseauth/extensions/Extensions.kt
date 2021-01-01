cf4c78ae3437afb4f35a49b5c63535a7f417731b
Ericgacoki <gacokieric@gmail.com>
2021-01-02 00:06:04 +0300

    code review-5

--- app/src/main/java/com/ericg/firebaseauth/extensions/Extensions.kt
+++ app/src/main/java/com/ericg/firebaseauth/extensions/Extensions.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.widget.Toast
 
 object Extensions {
+
     fun Activity.toast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
