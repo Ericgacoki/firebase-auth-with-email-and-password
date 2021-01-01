f5222b7f7ec68016b0ddab0c2bb9cdaeebb7d36d
Ericgacoki <gacokieric@gmail.com>
2021-01-02 00:05:48 +0300

    code review-4

--- app/src/main/java/com/ericg/firebaseauth/utils/FirebaseUtils.kt
+++ app/src/main/java/com/ericg/firebaseauth/utils/FirebaseUtils.kt
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 object FirebaseUtils {
+
     val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
 }
